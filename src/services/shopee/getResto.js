973d7bca641dd8dc0270c51bc9fdfac47016d3c9
sayakomo <skfgood2@outlook.com>
2024-12-19 13:08:30 +0700

    debug

--- src/services/shopee/getResto.js
+++ src/services/shopee/getResto.js
@@ -5,6 +5,7 @@ import { buildRequestData } from './helpers/buildRequestData.js';
 export const getResto = async (tikor) => {
     const api = "https://foody.shopee.co.id/api/buyer/listing-detail";
     const headers = {
+        "Content-Type": "application/json",
         "User-Agent": "Mozilla/5.0 (Linux; Android 13; Mi A1 Build/TQ3A.230705.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/114.0.5735.196 Mobile Safari/537.36"
     };
 
