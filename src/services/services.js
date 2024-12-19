5e3ac05b9ea2c25e7dabb743c35c8f0458275e73
sayakomo <skfgood2@outlook.com>
2024-12-19 11:22:50 +0700

    fix

--- src/services/services.js
+++ src/services/services.js
@@ -3,12 +3,12 @@ import { getResto } from './shopee/request.js';
 
 export const Shopee = async (device, tikor) => {
     try {
-        const expired = await isExpired(device);
+        const activate = await isExpired(device);
         
-        if (!expired) {
+        if (!activate) {
             return { status: 401, message: 'USER expired' }
         }
-
+        
         return getResto(tikor);
     } catch (error) {
         return {
