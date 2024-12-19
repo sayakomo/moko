ce575b849f3d5ca91c3af29b56906628a6f58d06
sayakomo <skfgood2@outlook.com>
2024-12-19 10:32:26 +0700

    fix

--- src/services/services.js
+++ src/services/services.js
@@ -1,7 +1,22 @@
-import { auth } from '../middlewares/auth.js';
+import { isExpired } from '../middlewares/auth.js';
+import { getResto } from './shopee/request.js';
 
 export const Shopee = async (device, tikor) => {
-}
+    try {
+        const expired = await isExpired(device);
+        
+        if (expired) {
+            return { status: 401, message: 'USER expired' }
+        }
+
+        return getResto(tikor);
+    } catch (error) {
+        return {
+            error: true,
+            message: error.message,
+        }
+    }
+};
 
 export const ShopeeFood = async (device, tikor, keyword) => {
 }
---
+++ src/services/shopee/request.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export const getResto = async (tikor) => {
+    return { message: 'Resto' };
+};
